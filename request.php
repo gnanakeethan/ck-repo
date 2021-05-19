e5a715e467b4ca0a006917f7d1ab4623080c3735
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2021-05-19 09:04:57 +0530

    Request function complete with simple tests added at last
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- request.php
+++ request.php
@@ -11,9 +11,23 @@ class SimpleJsonRequest
 				'content' => $data ? json_encode($data) : null,
 			],
 		];
-		$url .= ($parameters ? '?' . http_build_query($parameters) : '');
+		$url  .= ($parameters ? '?' . http_build_query($parameters) : '');
+		//create a new client
+		$redis = new Redis();
+		$redis->connect('127.0.0.1', '6379');
+		if (in_array($method, ['PUT', 'POST', 'PATCH', 'DELETE'])) {
+			return $redis->del('request_cache_' . $url);
+		}
+		if (!in_array($method, ['PUT', 'POST', 'PATCH', 'DELETE']) && $redis->exists('request_cache_' . $url)) {
+			// redis returns a string cached.
+			return $redis->get('request_cache_' . $url);
+		}
+		//get new contents. no cache found / cache invalidated by the request type
+		$cachableContent = file_get_contents($url, false, stream_context_create($opts));
+		$redis->set('request_cache_' . $url, $cachableContent, 3600);
 
-		return file_get_contents($url, false, stream_context_create($opts));
+		//return the string cached
+		return $cachableContent;
 	}
 
 	public static function get(string $url, array $parameters = null)
@@ -41,3 +55,13 @@ class SimpleJsonRequest
 		return json_decode(self::makeRequest('DELETE', $url, $parameters, $data));
 	}
 }
+
+$simple1 = SimpleJsonRequest::get('https://jsonplaceholder.typicode.com/todos/1');
+$simple2 = SimpleJsonRequest::get('https://jsonplaceholder.typicode.com/todos/2');
+$simple3 = SimpleJsonRequest::get('https://jsonplaceholder.typicode.com/todos/1');
+var_dump($simple1);
+var_dump($simple2);
+var_dump($simple3);
+$redis = new Redis();
+$redis->connect('127.0.0.1', '6379');
+var_dump($redis->keys('request_cache_*'));
